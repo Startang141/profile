b0add68fdbd41bd66d7e6918d05bd346ad8cb45b
startang141m <bintangadiyatma141@gmail.com>
2023-05-07 18:54:20 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Profile-App</title>
+</head>
+<body>
+    <section class="Profile">
+        <div class="Hero">
+            <div class="asset">
+            <img class="background" src="asset2.svg" alt="" srcset="">
+            <img class="propic" src="asset1.svg" alt="" srcset="">
+            </div>
+            <div class="content">
+                <div>
+                    <p class="subjudul">Nama</p>
+                    <h4>Bintang Adiyatma Agung Putra</h3>
+                </div>
+                <div>
+                    <p class="subjudul">My Story</p>
+                    <p class="text-story">Lörem ipsum vunade koduna megall teling. Homosm doheten kaktigt sas poskap. Redade. Öde ona fast antikadyngen. Polol antinese synera, i mar alltså renas. </p>
+                </div>
+                <div>
+                    <p class="subjudul">Personal Information</p>
+                    <div class="personal">
+                        <img src="User.svg" alt="" srcset="">
+                        <p>22 Tahun</p>
+                    </div>
+                    <div class="personal">
+                        <img src="Call.svg" alt="" srcset="">
+                        <p>082839948342</p>
+                    </div>
+                    <div class="personal">
+                        <img src="Message.svg" alt="" srcset="">
+                        <p>c066dsx3338@bangkit.academy</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="subjudul">Find Me</p>
+                    <div class="findme">
+                        <a href=""><img src="twitter.svg" alt=""></a>
+                        <a href=""><img src="github.svg" alt=""></a>
+                        <a href=""><img src="linkedin.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
